0ff6f8eb397eb431110726a9bf14f026c2a5af02
Mownika Chundu <chundumownika21@gmail.com>
2026-07-07 22:13:37 +0530

    Completed registration feature and custom user authentication service

--- src/main/java/com/mownika/jobportal/config/SecurityConfig.java
+++ src/main/java/com/mownika/jobportal/config/SecurityConfig.java
@@ -3,10 +3,15 @@ package com.mownika.jobportal.config;
 import com.mownika.jobportal.service.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import com.mownika.jobportal.service.CustomUserDetailsService;
 
 @Configuration
 public class SecurityConfig {
@@ -33,5 +38,17 @@ public class SecurityConfig {
 
     public SecurityConfig(CustomUserDetailsService customUserDetailsService) {
         this.customUserDetailsService = customUserDetailsService;
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+
+        provider.setUserDetailsService(customUserDetailsService);
+
+        provider.setPasswordEncoder(passwordEncoder());
+
+        return provider;
     }
 }
